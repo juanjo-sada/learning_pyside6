2d4f4cabf6e3cefe32550d06be1a422205504eed
Juanjo sada <juanjo.sada10@gmail.com>
2025-09-05 16:12:14 -0600

    Made some changes to signals_and_slots_1.py, played around with changing more properties of the app by triggering a signal

--- basic/signals_and_slots_1.py
+++ basic/signals_and_slots_1.py
@@ -1,4 +1,5 @@
 import sys
+from random import choice
 
 from PySide6.QtCore import QSize # Importing QSize to set a fixed size on our window
 from PySide6.QtWidgets import (
@@ -11,25 +12,18 @@ from PySide6.QtWidgets import (
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.button_is_checked = True
-
+        
         self.setWindowTitle("Main Window")
 
-        button = QPushButton("Main Button")
-        button.setCheckable(True)
-        button.clicked.connect(self.button_was_clicked)
-        button.clicked.connect(self.button_was_toggled)
-        button.setChecked(self.button_is_checked)
+        self.button_main = QPushButton("Main Button")
+        self.button_main.clicked.connect(self.button_was_clicked)
 
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button_main)
 
     def button_was_clicked(self):
-        print("Clicked!")
-
-    def button_was_toggled(self, checked):
-        self.button_is_checked = checked
-        print(f"Button is checked: {self.button_is_checked}")
+        self.button_main.setText("You clicked me, you can't undo it anymore")
+        self.button_main.setEnabled(False)
+        self.setWindowTitle("They have clicked the button")
 
 
 app = QApplication(sys.argv)
