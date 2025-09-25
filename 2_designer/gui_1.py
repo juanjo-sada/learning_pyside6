0694555dc82c7d2f5cbeec73436f1d7b030d02c9
Juanjo Sada <juanjo.sada10@gmail.com>
2025-09-24 21:04:44 -0600

    Renamed folders to keep track of order in which I'm learning them. Did my first GUI build and load into a program.

---
+++ 2_designer/gui_1.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+from PySide6 import QtWidgets
+from PySide6.QtUiTools import QUiLoader
+
+basedir = os.path.dirname(__file__)
+loader = QUiLoader()
+
+app = QtWidgets.QApplication(sys.argv)
+window = loader.load(os.path.join(basedir, "ui", "gui_1.ui"), parentWidget=None)
+window.show()
+app.exec()
