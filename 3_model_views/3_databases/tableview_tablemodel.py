ae774d17108f6063b55c2bc9af78ad7404e2214a
Juanjo Sada <juanjo.sada10@gmail.com>
2025-10-20 21:59:22 -0600

    Added comments to the TableModel example

--- 3_model_views/3_databases/tableview_tablemodel.py
+++ 3_model_views/3_databases/tableview_tablemodel.py
@@ -13,18 +13,27 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # Set the db to q SQLite database
         self.db = QSqlDatabase("QSQLITE")
+        # Select the db from the same directory as this file
         self.db.setDatabaseName(os.path.join(basedir, 'chinook.sqlite'))
+        # Open the db
         self.db.open()
 
         self.table = QTableView()
 
+        # Create a Table Model using the db
         self.model = QSqlTableModel(db=self.db)
 
         self.table.setModel(self.model)
 
+        # Choose what table to work with from the db. Our model only allows for a single table
+        # For working with multiple tables, use the QSqlRelationalTableModel
         self.model.setTable("Track")
         self.model.select()
+
+        # Set the editing strategy, this defines what triggers the changes made to be saved to the db
+        self.model.setEditStrategy(QSqlTableModel.OnRowChange)
 
         self.setMinimumSize(QSize(1024, 600))
         self.setCentralWidget(self.table)
